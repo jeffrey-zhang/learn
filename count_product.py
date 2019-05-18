85b40c376745abb5d405e4cdb8087a4e9bb38ebe
Jeffrey Zhang <jeffrey_zhang@trendmicro.com>
2019-05-18 15:05:54 +0800

    compare the list and set

--- count_product.py
+++ count_product.py
@@ -1,5 +1,5 @@
 import time
-id = [x for x in range(0,10000)]
+id = [x for x in range(0,100000)]
 price = [x for x in range(200000,300000)]
 priducts = list(zip(id,price))
 
@@ -15,8 +15,7 @@ def find_unique_price_using_list(products):
 def find_unique_price_using_set(products):
     unique_price_set = set()
     for _,price in products:
-        if price in products:
-            unique_price_set.add(price)
+        unique_price_set.add(price)
     return len(unique_price_set)
 
 start_using_list = time.perf_counter()
@@ -26,3 +25,5 @@ print('time elapse using list:{}'.format(end_using_list,start_using_list))
 
 start_using_set = time.perf_counter()
 find_unique_price_using_set(priducts)
+end_using_set = time.perf_counter()
+print('time elapse using set:{}'.format(end_using_set,start_using_set))
