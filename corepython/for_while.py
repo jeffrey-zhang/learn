786ec037fc576fd1268dab34adb2421419b417d4
Jeffrey Zhang <jeffrey_zhang@trendmicro.com>
2019-05-24 17:16:25 +0800

    add version c with build-in zip function

--- corepython/for_while.py
+++ corepython/for_while.py
@@ -7,20 +7,20 @@ values = [
 
 
 # version A
+print('Version A: 嵌套循环实现我们给定的功能')
 l = []
 for v in values:
     d = {}
     for i in range(0,len(attributes)):
         d[attributes[i]] = v[i]
     l.append(d)
-
-print('Version A: 嵌套循环实现我们给定的功能')
 print(l)
 
 
 # version B one line to implement the function.
-lx = [{attributes[i]:v[i] for i in range(0,len(attributes))} for v in values]
-
 print('Version B: 一行代码完成我们给定的功能')
-print(lx)
+print([{attributes[i]:v[i] for i in range(0,len(attributes))} for v in values])
 
+#version C one line to implement the function by build-in zip function
+print('Version C: 一行代码完成我们给定的功能（利用内置函数）')
+print([dict(zip(attributes,v))for v in values])
