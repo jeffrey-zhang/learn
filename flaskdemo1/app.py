22ee1290d844a3a8bef3664b225de7d4ce9df68a
Jeffrey Zhang <jeffrey.zhang@me.com>
2020-05-09 16:47:38 +0800

    m

--- flaskdemo1/app.py
+++ flaskdemo1/app.py
@@ -1,20 +1,31 @@
-from flask import Flask,url_for,redirect
+from flask import Flask,url_for,redirect, render_template
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    url1 = (url_for('news', id='10086'))
-    return redirect(url1)
+    return render_template("index.html")
 
-@app.route('/user/<name>')
-def user_name(name):
-    return 'Hello: %s' % name
+@app.route('/user/<username>')
+def user_name(username):
+    return render_template('user.html',name=username)
 
-@app.route('/news/<id>')
-def news(id):
-    return u'receive\'d id is %s' % id
+@app.route('/goods/list')
+def goods_list():
+    goods = [{'name':'怪味xxx','price':138.00},
+             {'name':'牛仔外衣','price':100},
+             {'name':'西装外套', 'price': 138},
+             {'name':' RICE 复古', 'price': 189}]
+    return render_template('goods.html',**locals())
+
+def do_index_class(index):
+    if index % 3 == 0:
+        return 'line'
+    else:
+        return ''
+    app.add_template_filter(do_index_class,'index_class')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
