8de06361552726c7ce8abe2296905ab6d83ada48
Jeffrey Zhang <jeffrey.zhang@me.com>
2020-08-25 17:01:41 +0800

    implement the simple search enginee

--- core_python_training/1_11/SearchEngineDemo.py
+++ core_python_training/1_11/SearchEngineDemo.py
@@ -17,11 +17,29 @@ class SearchEngineBase(object):
 
 def main(search_engine):
     for file_path in ['1.txt','2.txt','3.txt','4.txt','5.txt']:
-        search_engine.add_corpus(file_path)
+        search_engine.add_corpus('core_python_training/1_11/' + file_path)
 
     while True:
-        query = input()
+        query = input('Please input the word to search?\n')
         results = search_engine.search(query)
         print('found {} results'.format(len(results)))
         for result in results:
             print(result)
+
+class SimpleEnginee(SearchEngineBase):
+    def __init__(self):
+        super(SimpleEnginee, self).__init__()
+        self.__id_to_text = {}
+
+    def process_corpus(self, id, text):
+        self.__id_to_text[id] = text
+
+    def search(self, query):
+        results = []
+        for id,text in self.__id_to_text.items():
+            if query in text:
+                results.append(id)
+            return results
+
+search_engine = SimpleEnginee()
+main(search_engine)
--- core_python_training/1_11/SimpleEnginee.py
+++
@@ -1,5 +0,0 @@
-
-class SimpleEnginee(SearchEngineBase):
-    def __init__(self):
-        super(SimpleEnginee, self).__init__()
-        self.__id_to_text = {}
