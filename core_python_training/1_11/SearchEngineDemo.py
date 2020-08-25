65411a7ae46d5d6b738c6b1a6ab98727bebf9501
Jeffrey Zhang <jeffrey.zhang@me.com>
2020-08-25 17:29:15 +0800

    implement the simple search enginee2

--- core_python_training/1_11/SearchEngineDemo.py
+++ core_python_training/1_11/SearchEngineDemo.py
@@ -1,3 +1,5 @@
+import re
+
 class SearchEngineBase(object):
     def __init__(self):
         pass
@@ -42,4 +44,39 @@ class SimpleEnginee(SearchEngineBase):
             return results
 
 search_engine = SimpleEnginee()
+# main(search_engine)
+
+class BOWEngine(SearchEngineBase):
+    def __init__(self):
+        super(BOWEngine, self).__init__()
+        self.__id_to_words = {}
+
+    def process_corpus(self, id, text):
+        self.__id_to_words[id] = self.parse_text_to_words(text)
+
+    def search(self, query):
+        query_words = self.parse_text_to_words(query)
+        results = []
+        for id,words in query_words.items():
+            if self.query_match(query_words,words):
+                results.append(id)
+
+        return results
+
+    @staticmethod
+    def query_match(query_words,words):
+        for query_word in query_words:
+            if query_word not in words:
+                return False
+        return True
+
+    @staticmethod
+    def parse_text_to_words(text):
+        text = re.sub(r'[^\w]', ' ', text)
+        text = text.lower()
+        word_list = text.split(' ')
+        word_list = filter(None,word_list)
+        return set(word_list)
+
+search_engine = BOWEngine()
 main(search_engine)
