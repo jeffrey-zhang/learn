e5fa46db7f8f6adecdfc3160d22d6c7659b8318d
Bobo-Ren <69838140+IAlwaysLY@users.noreply.github.com>
2020-09-18 17:11:58 +0800

    Update PrintTriangle.java

--- training_program/02-java-part1/exam/bobo/src/com/nevstop/PrintTriangle.java
+++ training_program/02-java-part1/exam/bobo/src/com/nevstop/PrintTriangle.java
@@ -2,15 +2,6 @@ package com.nevstop;
 
 import java.util.Scanner;
 
-/**
- * description
-         现在我们一起来做一个项目，：首先大家是整体的一个团队，假设代码间存在依赖关系，相互存在调用关系。目前自己负责其中一个模块的开发，该模块的要求如下
-         1. 核心业务逻辑是打印出一个三角形
-         2. 存在跟用户（“外部接口调用”）交互，询问用户需要打印多少个三角形
-         3. 根据用户的需求，打印出满足条件的三角形
- * @author renbobo
- * @data 2020.9.18
- */
 public class PrintTriangle {
     /**
      * 打印三角形
