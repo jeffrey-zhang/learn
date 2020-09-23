2c3d0b1731f07f458e2ad6ac4b2e257d1da03403
yanxu <xinweihan_ets01@trendmicro.com>
2020-09-23 19:39:33 +0800

    add java pashe II sharing

---
+++ training_program/03-java-part2/sharing/JavaPhaseII/src/com/trendmicro/service/impl/AnimalServiceImpl.java
@@ -0,0 +1,12 @@
+package com.trendmicro.service.impl;
+
+import com.trendmicro.model.Animal;
+import com.trendmicro.service.AnimalService;
+
+public class AnimalServiceImpl implements AnimalService {
+
+    @Override
+    public void showColor(Animal animal){
+        System.out.println(animal.getName()+"是一只"+animal.getColor()+"色的"+animal.getKind());
+    }
+}
