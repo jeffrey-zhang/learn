c5758811213d4a4487c6f2dab469a5c93c8de0d7
yanxu <xinweihan_ets01@trendmicro.com>
2020-10-10 12:22:00 +0800

    modify exam 4 - auto match table column

--- training_program/04-java-part3/exam/YxExam/src/main/java/com/trendmicro/HandlerMain.java
+++ training_program/04-java-part3/exam/YxExam/src/main/java/com/trendmicro/HandlerMain.java
@@ -9,11 +9,9 @@ import java.sql.SQLException;
 public class HandlerMain {
 
     public static void main(String[] args) throws IOException, SQLException {
-
-        IHandlerFileService handleFileService= new HandlerFileServiceImpl();
+        IHandlerFileService handleFileService = new HandlerFileServiceImpl();
+        // 处理指定文件夹下的多个文件
         handleFileService.handlerMultiFile();
-
-
     }
 
 
--- training_program/04-java-part3/exam/YxExam/src/main/java/com/trendmicro/service/impl/HandlerFileServiceImpl.java
+++ training_program/04-java-part3/exam/YxExam/src/main/java/com/trendmicro/service/impl/HandlerFileServiceImpl.java
@@ -5,8 +5,13 @@ import com.trendmicro.service.IHandlerFileService;
 import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+/***
+ * 用户交流文件处理，插入数据库
+ */
 public class HandlerFileServiceImpl implements IHandlerFileService {
 
     private static final String driver = "com.mysql.cj.jdbc.Driver";
@@ -15,26 +20,28 @@ public class HandlerFileServiceImpl implements IHandlerFileService {
     private static final String password = "123456";
 
     @Override
-    public void handlerMultiFile() throws IOException, SQLException {
+    public void handlerMultiFile() throws IOException {
+        System.out.println("多线程处理文件 开始 ...");
         final String path = this.getClass().getResource("/").getPath() + "FileModel";
-        System.out.println(path);
+        System.out.println("目录：" + path);
         File file = new File(path);
         if (file.exists()) {
             if (file.listFiles() == null) {
                 return;
             }
-            final File[] files = file.listFiles();
-            System.out.println(files.length);
+            File[] files = file.listFiles();
+            System.out.println("目录中包含文件数量：" + files.length);
             for (int i = 0; i < files.length; i++) {
                 if (files[i].isFile()) {
                     final int finalI = i;
+                    final String filePath = path + "/" + files[finalI].getName();
                     new Thread(new Runnable() {
                         @Override
                         public void run() {
-                            String filePath = path+"/"+files[finalI].getName();
-                            try{
-                                handlerFile(finalI,filePath);
-                            }catch (Exception e){
+                            try {
+                                System.out.println("开始执行线程" + finalI + "...");
+                                handlerFile(finalI, filePath);
+                            } catch (Exception e) {
                                 e.printStackTrace();
                             }
 
@@ -44,11 +51,12 @@ public class HandlerFileServiceImpl implements IHandlerFileService {
             }
         }
     }
+
     @Override
-    public void handlerFile(int thread,String filepath) throws IOException, SQLException {
-        Connection conn = null;
+    public void handlerFile(Integer thread, String filepath) throws IOException, SQLException {
         FileInputStream inputStream = null;
         BufferedReader bufferedReader = null;
+        Connection conn = null;
         try {
             conn = getConnection();
             File file = new File(filepath);
@@ -57,17 +65,32 @@ public class HandlerFileServiceImpl implements IHandlerFileService {
             String strLine = "";
             String tableName = file.getName().replace(".csv", "").replace("-", "");
             int count = 1;
+            Map<Integer, String> tableColumnMap = new HashMap<Integer, String>();
+            // 表的列名
+            String columnNames = "";
+            // 用于储存 生成多少个问号
+            String columnValue = "";
+            // 统计多少个列
+            int columnCount = 0;
             while ((strLine = bufferedReader.readLine()) != null) {
                 if (count == 1) {
                     // 向DB新增表
-                    createTable(conn,tableName,strLine);
-                    System.out.println("创建表完成。");
+                    tableColumnMap = createTable(conn, tableName, strLine);
+                    System.out.println("线程" + thread + ":创建表(" + tableName + ")完成。");
+                    // 处理 表 列的值和个数
+                    for (Map.Entry<Integer, String> entry : tableColumnMap.entrySet()) {
+                        columnNames = entry.getValue();
+                        columnCount = entry.getKey();
+                        for (int i = 0; i < entry.getKey(); i++) {
+                            columnValue += "?,";
+                        }
+                        // 去掉最后一个逗号
+                        columnValue = columnValue.substring(0, columnValue.length() - 1);
+                    }
                 } else {
+                    // System.out.println("线程"+thread+"，执行插入第"+count+"行。");
                     //  插入数据
-                    insertData(conn, tableName, strLine);
-                    System.out.println("线程"+thread+"，插入第"+count+"行。");
-                    // 测试线程
-                   // System.out.println("第"+count+"行："+strLine);
+                    insertData(conn, tableName, strLine, columnNames, columnValue, columnCount, thread, count);
                 }
                 count++;
             }
@@ -80,14 +103,24 @@ public class HandlerFileServiceImpl implements IHandlerFileService {
             if (bufferedReader != null) {
                 bufferedReader.close();
             }
-            if(conn!=null){
+            if (conn != null) {
                 conn.close();
             }
-            System.out.println("线程"+thread+"处理结束。");
+            System.out.println("线程" + thread + "处理结束。");
         }
     }
 
-    private void createTable(Connection conn,String tableName,String strLine)throws SQLException {
+    /**
+     * 向DB新建表
+     *
+     * @param conn
+     * @param tableName
+     * @param strLine
+     * @return
+     * @throws SQLException
+     */
+    private Map<Integer, String> createTable(Connection conn, String tableName, String strLine) throws SQLException {
+        Map<Integer, String> map = new HashMap<Integer, String>();
         String createTableSql = "create table if not exists " + tableName + " (id int(4) not null primary key auto_increment";
         //  去掉第一个 无效的字符
         strLine = strLine.replace("\ufeff", "");
@@ -96,22 +129,36 @@ public class HandlerFileServiceImpl implements IHandlerFileService {
             createTableSql += "," + (fields[i]) + " varchar(300)";
         }
         createTableSql += ")";
+        map.put(fields.length, strLine);
         PreparedStatement statement = conn.prepareStatement(createTableSql);
         // 执行
         statement.executeUpdate();
         // 关闭
         statement.close();
+        return map;
 
     }
 
-    private void insertData(Connection conn, String tableName, String strLine) throws SQLException {
-        String insertData = "insert into " + tableName + " (Time,Region,UserName,ETSMember,ETSServiceAuto,ETSQuestionAuto,AnswerHit,ETSQuesionManual,UserSays,UserSaysLower,JARVISAnswerKB,KBpageId,KBurl,KBWikiPosition,JARVISAnswerForm,SessionId) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    /**
+     * 向DB插入数据
+     *
+     * @param conn
+     * @param tableName
+     * @param strLine
+     * @param columnNames
+     * @param columnValue
+     * @param columnCount
+     * @throws SQLException
+     */
+    private void insertData(Connection conn, String tableName, String strLine, String columnNames, String columnValue, Integer columnCount, Integer thread, Integer lineCount) throws SQLException {
+        String insertData = "insert into " + tableName + " (" + columnNames + ") values(" + columnValue + ")";
         // 过滤掉单元格内存在的逗号
         String newStrLine = filterString(strLine);
         // 根据逗号区分各单元格内容
         String[] lineData = newStrLine.split(",");
         // 不符合规范的数据 直接略过不处理
-        if (lineData.length != 16) {
+        if (lineData.length != columnCount) {
+            System.out.println("线程" + thread + ":第" + lineCount + "行数据不符合规范略过。");
             return;
         }
         PreparedStatement statement = conn.prepareStatement(insertData);
@@ -128,6 +175,12 @@ public class HandlerFileServiceImpl implements IHandlerFileService {
         statement.close();
     }
 
+    /**
+     * 过滤掉 单元格内容中的逗号，先替换为&xx&,拆分完毕后在替换为逗号。
+     *
+     * @param content
+     * @return
+     */
     private String filterString(String content) {
         String newContent = content;
         List<Integer> indexList = new ArrayList<Integer>();
@@ -142,7 +195,7 @@ public class HandlerFileServiceImpl implements IHandlerFileService {
         for (int i = 0; i < foreachNumber; i++) {
             int beginIndex = indexList.get(i * 2);
             int endIndex = indexList.get(i * 2 + 1) + 1;
-            //System.out.println("beginIndex:"+beginIndex+",endIndex:"+endIndex);
+            // System.out.println("beginIndex:"+beginIndex+",endIndex:"+endIndex);
             String stringCon = content.substring(beginIndex, endIndex);
             newContent = newContent.replace(stringCon, stringCon.replace(",", "&xx&"));
         }
@@ -159,17 +212,5 @@ public class HandlerFileServiceImpl implements IHandlerFileService {
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
-    }
-
-    public void closeAll(Connection conn, PreparedStatement stmt, ResultSet rs) throws SQLException {
-        if (rs != null) {
-            rs.close();
-        }
-        if (stmt != null) {
-            stmt.close();
-        }
-        if (conn != null) {
-            conn.close();
-        }
     }
 }
