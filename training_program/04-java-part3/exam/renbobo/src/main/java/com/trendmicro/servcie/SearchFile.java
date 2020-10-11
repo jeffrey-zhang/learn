468e95a341b8e64384d4e6b3dc3dff4700186354
xwh_ets_bobo_ren <xwh_ets_bobo_ren@trendmicro.com>
2020-10-11 18:53:21 +0800

    update my project

--- training_program/04-java-part3/exam/renbobo/src/main/java/com/trendmicro/servcie/SearchFile.java
+++ training_program/04-java-part3/exam/renbobo/src/main/java/com/trendmicro/servcie/SearchFile.java
@@ -1,2 +1,8 @@
-package com.trendmicro.servcie;public interface searchFile {
+package com.trendmicro.servcie;
+
+import java.io.File;
+import java.util.List;
+
+public interface SearchFile {
+    List<String> searchFile(File file);
 }
--- training_program/04-java-part3/exam/renbobo/src/main/java/com/trendmicro/servcie/impl/SearFileImpl.java
+++ training_program/04-java-part3/exam/renbobo/src/main/java/com/trendmicro/servcie/impl/SearFileImpl.java
@@ -1,2 +1,17 @@
-package com.trendmicro.servcie.impl;public class SearFileImpl {
+package com.trendmicro.servcie.impl;
+
+import com.trendmicro.servcie.SearchFile;
+
+import java.io.File;
+import java.util.List;
+
+public class SearFileImpl implements SearchFile {
+
+
+    @Override
+    public List<String> searchFile(File file) {
+
+
+        return null;
+    }
 }
